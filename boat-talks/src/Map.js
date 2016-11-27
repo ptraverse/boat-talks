3781df560c7fa0d572096f039f17e52d86070655
Philippe Traverse <philippe.traverse@gmail.com>
2016-11-27 21:57:08 +0000

    Pushing c9 code to local VM

--- boat-talks/src/Map.js
+++ boat-talks/src/Map.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './Map.css'
+/* global L, JNC, $ */
 
 class Map extends Component {
-  render() {
-    return (
-      <div className="Map" id="map">
-        TODO Add JS to build the map here
-      </div>
-    );
-  }
+    componentDidMount() {
+      
+        $('#map').focus();
+      
+        var userCenter = [49.1, -121.2];
+        var zoom = 5;
+          
+        var map = L.map('map');
+        map.setView(userCenter, zoom);
+        
+        var overlay = new JNC.Leaflet.NavionicsOverlay({
+            navKey: 'Navionics_webapi_02834',
+            chartType: JNC.NAVIONICS_CHARTS.NAUTICAL,
+            isTransparent: false,
+            zIndex: 1
+        });
+        overlay.addTo(map);
+        L.marker(userCenter).addTo(map);
+    }
+  
+    render() {
+        return (
+            <div id="map">
+            </div>
+        );
+    }
+
+    
 }
 
 export default Map;
