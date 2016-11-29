3113b59e53fba92232bba4147aa62403273c09e3
Philippe Traverse <philippe.traverse@gmail.com>
2016-11-29 01:12:32 -0800

    Using backup map from navionics, will use them later

--- src/Map.js
+++ src/Map.js
@@ -7,19 +7,30 @@ class Map extends Component {
       
         $('#map').focus();
       
-        var userCenter = [49.1, -121.2];
-        var zoom = 5;
+        var userCenter = [49.2, -123.5];
+        var zoom = 10;
           
         var map = L.map('map');
         map.setView(userCenter, zoom);
         
-        var overlay = new JNC.Leaflet.NavionicsOverlay({
-            navKey: 'Navionics_webapi_02834',
-            chartType: JNC.NAVIONICS_CHARTS.NAUTICAL,
-            isTransparent: false,
-            zIndex: 1
+        // var overlay = new JNC.Leaflet.NavionicsOverlay({
+        //     navKey: 'Navionics_webapi_02834',
+        //     chartType: JNC.NAVIONICS_CHARTS.NAUTICAL,
+        //     isTransparent: false,
+        //     zIndex: 1
+        // });
+        // overlay.addTo(map);
+
+        var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         });
-        overlay.addTo(map);
+        var OpenSeaMap = L.tileLayer('http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png', {
+            attribution: 'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors'
+        });
+        OpenStreetMap_Mapnik.addTo(map);
+        OpenSeaMap.addTo(map);
+
         L.marker(userCenter).addTo(map);
     }
   
