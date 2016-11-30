921085bea41be5892358773b2bc82d277225b720
Philippe Traverse <philippe.traverse@gmail.com>
2016-11-30 01:27:07 -0800

    Ready to attempt to send back to c9

--- src/Map.js
+++ src/Map.js
@@ -1,35 +1,49 @@
 import React, { Component } from 'react';
-import './Map.css'
+import './Map.css';
 /* global L, JNC, $ */
 
-class Map extends Component {
+class Map extends Component {        
+    
+
     componentDidMount() {
       
+        function getRandom(min, max) {
+          return Math.random() * (max - min) + min;
+        }
+
         $('#map').focus();
       
         var userCenter = [49.2, -123.5];
+        var skew = true;        
+        if (skew) {
+            var xAdj = 0.25;
+            var yAdj = 0.5;
+            userCenter[0] = getRandom(userCenter[0] - xAdj, userCenter[0] + xAdj);
+            userCenter[1] = getRandom(userCenter[1] - yAdj, userCenter[1] + yAdj);
+        }
         var zoom = 10;
           
         var map = L.map('map');
         map.setView(userCenter, zoom);
         
-        // var overlay = new JNC.Leaflet.NavionicsOverlay({
-        //     navKey: 'Navionics_webapi_02834',
-        //     chartType: JNC.NAVIONICS_CHARTS.NAUTICAL,
-        //     isTransparent: false,
-        //     zIndex: 1
-        // });
-        // overlay.addTo(map);
+        var overlay = new JNC.Leaflet.LeafletNavionicsOverlay({
+            navKey: 'Navionics_webapi_02834',
+            chartType: JNC.NAVIONICS_CHARTS.NAUTICAL,
+            isTransparent: false,
+            zIndex: 1
+        });
+        overlay.addTo(map);
 
-        var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        });
-        var OpenSeaMap = L.tileLayer('http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png', {
-            attribution: 'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors'
-        });
-        OpenStreetMap_Mapnik.addTo(map);
-        OpenSeaMap.addTo(map);
+        // var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //     maxZoom: 19,
+        //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        // });
+        // OpenStreetMap_Mapnik.addTo(map);
+        
+        // var OpenSeaMap = L.tileLayer('http://t1.openseamap.org/seamark/{z}/{x}/{y}.png', {
+        //     attribution: 'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors'
+        // });        
+        // OpenSeaMap.addTo(map);
 
         L.marker(userCenter).addTo(map);
     }
