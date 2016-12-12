3ccf25d93c409bf8dc1ea75ff4105bf7e5fbfe7b
Philippe Traverse <philippet@tribalworldwide.ca>
2016-12-11 23:52:15 -0800

    Starting scraping script

---
+++ scripts/scrape-tide-locations.js
@@ -0,0 +1,9 @@
+/**
+ * Going to BORROW tide locations from http://tides.mobilegeographics.com/index.html
+ * With an eye to using the graph on each location page,
+ * e.g. http://tides.mobilegeographics.com/locations/5859.html
+ * Mongodb collection 'bt.tidelocations':
+ *  Fields: Name, Location Number, Lat, Lon
+ * From that, the map can show an icon, and when user clicks icon, server can fetch (and cache) the graph...
+**/
+
